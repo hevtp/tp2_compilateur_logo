70723052595122a17ce69b722e279aabafca08ea
hevtp <hevisindatop@gmail.com>
2026-03-27 11:17:04 +0100

    Premier commit et epartie 1

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//! TP2 - Compilateur et Interpréteur Logo par Agathe Julien et Hevisinda Top
+//! Objectif : faire revivre le language logo en créeant un compliateur logo vers svg, puis un intérprateur logo.
+//!
+//!
+fn main() {
+    println!("Hello, world!");
+}
